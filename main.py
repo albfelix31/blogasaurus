5a6284747964017624b4e17873f0fb00d76a8069
Demo User <demouser@MacBook-Air.local>
2017-07-25 17:27:41 -0400

    Hello

--- main.py
+++ main.py
@@ -16,57 +16,16 @@
 #
 import webapp2
 import logging
+import jinja2
+import os
 
-HOMEPAGE = """
-<html>
-  <head>
-    <title> Blogasaurus </title>
-    <link rel="stylesheet" href="/resources/MyCSS.css">
-  </head>
-  <body>
-    <h1 id = "blogTitle"> Blogasaurus </h1>
-
-    <h2> Who am I? </h2>
-
-    <p> Well hello there, awesome reader of this super amazing blog of mine! <br>
-      My name is Albert Felix, and yeah, sometimes I wonder who I am, too. <br>
-      But first things first, what's your name?
-      <br>
-      I like photography and drawing.
-      I'll be attending the Macaulay Honors College at CCNY
-      this fall.
-    </p>
-    </body>
-</html>
-
-"""
-
-# def is_prime(n):
-# #"""A simple (but inefficient) check to see whether a number is prime."""
-#     for possible_factor in range(2, n):
-#         logging.info('trying %d' % possible_factor)
-#         if n % possible_factor == 0:
-#             return False
-#     return True
-
+jinja_environment = jinja2.Environment(loader=
+    jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write(HOMEPAGE)
-
-
-# class CssiHandler(webapp2.RequestHandler):
-#     def get(self):
-#         self.response.write('Hello CSSI!')
-#
-# class CountHandler(webapp2.RequestHandler):
-#     def get(self):
-#         for i in range(1, 21):
-#             self.response.write('Hello %d <br>' %i)
-#
-# class GoodbyeHandler(webapp2.RequestHandler):
-#     def get(self):
-#         self.response.write('Goodbye')
+        template = jinja_environment.get_template('Blogasaurus.html')
+        self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
