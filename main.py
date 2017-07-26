92407f8ae044385d5b654cf9a3906c745ffc91b3
Demo User <demouser@MacBook-Air.local>
2017-07-26 09:43:16 -0400

    Hi

--- main.py
+++ main.py
@@ -27,6 +27,12 @@ class MainHandler(webapp2.RequestHandler):
         template = jinja_environment.get_template('Blogasaurus.html')
         self.response.write(template.render())
 
+class SurveyHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('survey.html')
+        self.response.write(template.render())
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
+    ('/survey', SurveyHandler),
 ], debug=True)
